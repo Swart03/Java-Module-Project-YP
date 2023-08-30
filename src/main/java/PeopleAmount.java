cc9abe75ed7e4d4692f28ac95c7639ddeea59adf
Swart03 <swart06@gmail.com>
2023-08-30 10:06:47 +0300

    PeopleAmount.java

--- src/main/java/PeopleAmount.java
+++ src/main/java/PeopleAmount.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
+
 public class PeopleAmount {
+int readPeopleAmount(Scanner scanner) {
+    int amount;
+    System.out.println("На скольких человек делим счёт?");
+    while(true) {
+        if (scanner.hasNextInt()) {
+            amount = scanner.nextInt();
+            if (amount <= 1) {
+                System.out.println("Количество человек должно быть больше одного.");
+            } else {
+                scanner.nextLine();
+                return amount;
+            }
+        } else {
+            System.out.println("Количпество человек болжно быть введено в виде числа.");
+            scanner.next();
+        }
+    }
+
+}
+
 }
