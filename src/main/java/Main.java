c88b19f0b2c3084d9f60ba63ac825a73f29f7816
Swart03 <swart06@gmail.com>
2023-08-30 09:05:22 +0300

    Тест

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,30 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        int peopleAmount;
+        peopleAmount = scanner.nextInt();
+        System.out.println("На скольких человек нужно разделить счет?");//"На скольких человек нужно разделить счет?"
+        //Неверное количество гостей. Количество гостей должно быть введено в виде цифрового значения, и гостей должно быть больше одного.
+        while(true){
+            System.out.println("На скольких человек нужно разделить счет?");
+            if(scanner.hasNextInt()) {
+             if(peopleAmount > 1);
+             break;
+            } else {
+                System.out.println("Неверное количество гостей. Количество гостей должно быть введено в виде цифрового значения, " +
+                        "и гостей должно быть больше одного.");
+                System.out.println("На скольких человек нужно разделить счет?");
+            }
+
+
+
+        }
+
+
+
+
+
     }
 }
